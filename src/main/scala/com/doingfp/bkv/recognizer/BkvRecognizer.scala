f82e97f10cab0d8928e3ceb9dcd3d7585a2bbf34
blackopseng <pavel@blackopsdev.com>
2015-09-21 20:08:48 +0300

    Grammar described

--- src/main/scala/com/doingfp/bkv/recognizer/BkvRecognizer.scala
+++ src/main/scala/com/doingfp/bkv/recognizer/BkvRecognizer.scala
@@ -1,71 +1,61 @@
 package com.doingfp.bkv.recognizer
 
 import org.parboiled2._
-import CharPredicate.AlphaNum
-
 
 class BkvRecognizer(val input: ParserInput) extends Parser {
 
+  val WhitespaceChars = "\n\t "
+  val KeySymbol = CharPredicate.AlphaNum ++ '_'
+
   def WhiteSpace = rule {
-    anyOf(" \n\t")
+    anyOf(WhitespaceChars)
   }
 
-  def OptionalWhiteSpaces = rule {
+  def MayBeWS = rule {
     zeroOrMore(WhiteSpace)
   }
 
-  /**
-   * At least one whitespace is required
-   * Требуется хотя бы один пробел
-   */
-  def RequiredWhiteSpaces = rule {
-    oneOrMore(WhiteSpace)
-  }
-
   def NewLine = rule {
-    '\r'.? ~ '\n'
+    optional('\r') ~ '\n'
   }
 
   def Key = rule {
-    oneOrMore(AlphaNum ++ "_")
+    oneOrMore(KeySymbol)
   }
 
-  /**
-   * May contain Any character, except whitespace and newline. They are not allowed because:
-   *  - Newline separates one pair from another
-   *  - Whitespaces may present before the Value,
-   *
-   * Может содержать Любой (Any) символ за исключением пробела и перевода строки. Данные символы запрещены потому что:
-   *  - Перевод строки разделяет одну пару символов от другой
-   *  - Множетсво пробелов может предшествовать Значению (Value)
-   */
+  def ValueChar = rule {
+    noneOf(WhitespaceChars)
+  }
+
   def Value = rule {
-    CharPredicate.Visible.+
+    oneOrMore(ValueChar)
   }
 
   def KeyValuePair = rule {
-    Key ~ OptionalWhiteSpaces ~ "=" ~ OptionalWhiteSpaces ~ Value
+    Key ~ MayBeWS ~ "=" ~ MayBeWS ~ Value
   }
 
-  /**
-   * Rule must be explicitly specified here. In this case it will be Rule0 (we don't extract any value).
-   * Правило должно быть явно объявлено здесь. В данном случае это будет Rule0, (так как мы не извлекаем значения).
-   * @return
-   */
+  def KeyValueLine = rule {
+    MayBeWS ~ KeyValuePair ~ MayBeWS
+  }
+
   def Node: Rule0 = rule {
-    KeyValuePair | Block
+    KeyValueLine | Block
+  }
+
+  def Nodes = rule {
+    oneOrMore(Node).separatedBy(NewLine)
+  }
+
+  def BlockName = rule {
+    MayBeWS ~ Key ~ MayBeWS
   }
 
   def Block = rule {
-    Key ~ OptionalWhiteSpaces ~ "{" ~ oneOrMore(OptionalWhiteSpaces ~ Node).separatedBy(NewLine) ~ "}"
+    BlockName ~ "{" ~ Nodes ~ "}" ~ MayBeWS
   }
 
-
-  /**
-   * Entry point
-   * Точка входа
-   */
   def Root = rule {
-    oneOrMore(Node).separatedBy(NewLine)
+    Nodes
   }
 }
