50e46fb8c3b7b7e34a9978949a35d8b3678c5e49
Paul Popoff <oc14@ya.ru>
2015-10-26 14:48:18 +0300

    initial commit

---
+++ src/test/scala/com/doingfp/bkv/parser/BkvParserSpec.scala
@@ -0,0 +1,100 @@
+package com.doingfp.bkv.parser
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{FunSpec, Matchers}
+
+
+@RunWith(classOf[JUnitRunner])
+class BkvParserSpec extends FunSpec with Matchers {
+  describe ("Companion object") {
+    describe ("Key symbol") {
+      it ("should not contain whitespace characters") {
+        BkvParser.KeySymbol.matchesAny(" \t\n") shouldBe false
+      }
+
+      it ("may contain any alphanumeric symbol, or underscore") {
+        BkvParser.KeySymbol.matchesAll("AaZz0123456789_") shouldBe true
+      }
+
+      it ("should fail for other printable symbols") {
+        BkvParser.BlockNameSymbol.matchesAny(";/#-=(){}[]`!") shouldBe false
+      }
+    }
+
+    describe("Block name symbol") {
+      it ("should not contain whitespace characters") {
+        BkvParser.BlockNameSymbol.matchesAny(" \t\n") shouldBe false
+      }
+
+      it ("may contain any alphanumeric symbol, or underscore, or dot") {
+        BkvParser.BlockNameSymbol.matchesAll("AaZz0123456789_.") shouldBe true
+      }
+
+      it ("should fail for other printable symbols") {
+        BkvParser.BlockNameSymbol.matchesAny(";/#-=(){}[]`!") shouldBe false
+      }
+    }
+  }
+
+  describe ("Bkv Recognizer rules") {
+    describe("WhiteSpace characters") {
+      it ("should fail on empty input") {
+        new BkvParser("").WhiteSpace.run().isFailure shouldBe true
+      }
+
+      it ("should recognize whitespace char") {
+        new BkvParser(" ").WhiteSpace.run().isSuccess shouldBe true
+      }
+
+      it ("should recognize \\n as whitespace char") {
+        new BkvParser("\n").WhiteSpace.run().isSuccess shouldBe true
+      }
+
+      it ("should recognize \\t as whitespace char") {
+        new BkvParser("\t").WhiteSpace.run().isSuccess shouldBe true
+      }
+    }
+
+    // todo: check are they fake?
+    describe("Optional whitespaces") {
+      describe ("matches zero or more whitespace chars") {
+        it ("should match no whitespace chars at all") {
+          new BkvParser("").MayBeWS.run().isSuccess shouldBe true
+        }
+
+        it ("should match a single whitespace char") {
+          new BkvParser(" ").MayBeWS.run().isSuccess shouldBe true
+        }
+
+        it ("should match a multiple whitespace chars") {
+          new BkvParser("   ").MayBeWS.run().isSuccess shouldBe true
+        }
+
+        it ("should match different combinations of whitespace characters") {
+          new BkvParser("\\t\\n  ").MayBeWS.run().isSuccess shouldBe true
+        }
+      }
+    }
+
+    describe("Newline") {
+      describe("Unix/Linux newline") {
+        it ("should match unix/linux newline") {
+          new BkvParser("\n").NewLine.run().isSuccess shouldBe true
+        }
+
+        it ("should match Windows type of newline") {
+          new BkvParser("\r\n").NewLine.run().isSuccess shouldBe true
+        }
+      }
+    }
+
+//    //todo: are they fake? add EOI somewhere
+    describe("Key") {
+      it ("should follow the same rules as identifier") {
+//        new BkvParser("valid_Key0123").Key.run().isSuccess shouldBe true
+        new BkvParser("invalid Key0123").Key.run().isSuccess shouldBe true
+      }
+    }
+  }
+}
