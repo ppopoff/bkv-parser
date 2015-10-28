afd1b36a99be15b1d69ed66e94914d477f6fbadb
Paul Popoff <oc14@ya.ru>
2015-10-28 16:26:50 +0300

    initial commit

---
+++ src/test/scala/com/doingfp/bkv/parser/NodeAccessDslTest.scala
@@ -0,0 +1,11 @@
+package com.doingfp.bkv.parser
+
+import org.junit.runner.RunWith
+import org.scalatest.{FunSpec, Matchers}
+import org.scalatest.junit.JUnitRunner
+
+
+@RunWith(classOf[JUnitRunner])
+class NodeAccessDslTest extends FunSpec with Matchers {
+  //TODO
+}
