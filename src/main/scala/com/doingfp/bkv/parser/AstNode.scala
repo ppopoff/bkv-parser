ebd1d569e3936879ec64c35edc3b400a3b3a17c6
Paul Popoff <oc14@ya.ru>
2015-10-29 01:20:33 +0300

    code was simplified -> flag isBlockNode was removed

--- src/main/scala/com/doingfp/bkv/parser/AstNode.scala
+++ src/main/scala/com/doingfp/bkv/parser/AstNode.scala
@@ -2,15 +2,10 @@ package com.doingfp.bkv.parser
 
 sealed trait AstNode extends NodeAccessDsl {
   def name: String
-  def isBlockNode: Boolean
 }
 
+case class KeyValueNode
+(override val name: String, value: String) extends AstNode
 
-case class KeyValueNode(override val name: String, value: String) extends AstNode {
-  override val isBlockNode = false
-}
-
-
-case class BlockNode(override val name: String, nodes: Seq[AstNode]) extends AstNode {
-  override val isBlockNode = true
-}
+case class BlockNode
+(override val name: String, nodes: Seq[AstNode]) extends AstNode
