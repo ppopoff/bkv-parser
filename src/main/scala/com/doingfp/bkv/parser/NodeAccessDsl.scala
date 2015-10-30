1947c9741884c00209a1ae2724f42b8148dd59e6
Paul Popoff <oc14@ya.ru>
2015-10-30 13:44:28 +0300

    comments added, renamings, old variant removed

--- src/main/scala/com/doingfp/bkv/parser/NodeAccessDsl.scala
+++ src/main/scala/com/doingfp/bkv/parser/NodeAccessDsl.scala
@@ -3,6 +3,7 @@ package com.doingfp.bkv.parser
 trait NodeAccessDsl { this: AstNode =>
 
   def isRoot = this.name == BkvParser.RootNodeName
+
   lazy val isKeyValuePair = !isBlockNode
   lazy val isBlockNode = this match {
     case _: KeyValueNode => false
@@ -14,35 +15,35 @@ trait NodeAccessDsl { this: AstNode =>
    * @return a sequence of nested key value pairs
    */
   def pairs: Seq[KeyValueNode] = this match {
-    case BlockNode(name, nodes) =>
+    case BlockNode(_, nodes) =>
       nodes collect { case node: KeyValueNode => node }
     case _ => Seq.empty
   }
 
 
   /**
-   * @param name name of the blocks we need
+   * @param key of the blocks we need
    * @return a sequence of key-value pair nodes that have
    *         specified name
    */
-  def pairs(name: String): Seq[KeyValueNode] =
-    pairs filter (_.name == name)
+  def pairs(key: String): Seq[KeyValueNode] =
+    pairs filter (_.name == key)
 
 
-  //TODO comment
   /**
-   * @param name
-   * @return
+   * @param key of the pair we're looking for
+   * @return the first pair with specified name
+   *         (if exists)
    */
-  def pair(name: String): Option[KeyValueNode] =
-    pairs find (_.name == name)
+  def pair(key: String): Option[KeyValueNode] =
+    pairs find (_.name == key)
 
 
   /**
    * @return a sequence of nested block nodes
    */
   def blocks: Seq[BlockNode] = this match {
-    case BlockNode(name, nodes) =>
+    case BlockNode(_, nodes) =>
       nodes collect { case node: BlockNode => node }
     case _ => Seq.empty
   }
@@ -55,11 +56,11 @@ trait NodeAccessDsl { this: AstNode =>
   def blocks(name: String): Seq[BlockNode] =
     blocks filter (_.name == name)
 
-  //TODO: Comment
+
   /**
-   *
-   * @param name
-   * @return
+   * @param name - of the sought-for block
+   * @return the first block with specified name
+   *         (if exists)
    */
   def block(name: String): Option[BlockNode] =
     blocks find (_.name == name)
@@ -72,14 +73,4 @@ trait NodeAccessDsl { this: AstNode =>
     case KeyValueNode(_, value) => Some(value)
     case _ => None
   }
-
-
-  def get(nodename: String): Option[AstNode] = this match {
-    case BlockNode(name, values) =>
-      values find (_.name == nodename)
-    case KeyValueNode(_, _) =>
-      None
-  }
-
-  def / (nodename: String) = get(nodename)
 }
